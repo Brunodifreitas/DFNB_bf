d2980262acae0b4d723197f490755ce49a3da0d9
Bruno Freitas <bf297@ldsbc.edu>
2020-11-30 11:50:21 -0700

    Added Version 2 enhancements for LDSBC IT 240 - Project 1.6: DFNB - Analyze History

--- other_scripts/LoadDFNB_p1.sql
+++ other_scripts/LoadDFNB_p1.sql
@@ -11,6 +11,7 @@ Ver      Date        Author        Description
 -----   ----------   -----------   -------------------------------------------------------------------------------
 1.0     10/21/2020   BRUNOFREITAS  1. Creation of the Script
 1.1     11/04/2020   BRUNOFREITAS  1. Creation of loads 
+2.0		11/30/2020	 BRUNOFREITAS  1. Improvements Version 2
 RUNTIME: 
 Approx. 1 min
 
@@ -24,18 +25,17 @@ Loading DFNB_STG_P1 - Project 1 database. Meaning creating all the tables inside
 TRUNCATE TABLE dbo.tblCustomerDim;
 
 INSERT INTO dbo.tblCustomerDim
-
-SELECT DISTINCT
+SELECT DISTINCT 
        s.cust_id
      , s.first_name
      , s.last_name
      , s.gender
-	 , s.birth_date
+     , s.birth_date
      , s.cust_since_date
      , s.cust_add_id
-	 , s.pri_branch_id
-	 , s.cust_rel_id
-FROM dbo.stg_p1 as s
+     , s.pri_branch_id
+     , s.cust_rel_id
+  FROM dbo.stg_p1 AS s;
 
 -- 2) Load dbo.tblProductDim.sql
 
@@ -127,11 +127,11 @@ SELECT DISTINCT
 	 , s.acct_cust_role_id AS cust_role_id
   FROM dbo.stg_p1 AS s;
 
--- 10) Load dbo.tblAccountFactDim.sql
+-- 10) Load dbo.tblAccountFact.sql
 
-TRUNCATE TABLE dbo.tblAccountFactDim;
+TRUNCATE TABLE dbo.tblAccountFact;
 
-INSERT INTO dbo.tblAccountFactDim
+INSERT INTO dbo.tblAccountFact
 SELECT DISTINCT 
        s.as_of_date
 	 , s.cur_bal
